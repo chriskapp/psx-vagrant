4d98076c016dd018a966e27c2b83d3bc62a74e8a
Christoph Kappestein <k42b3.x@gmail.com>
2015-01-04 16:05:57 +0100

    remove public folder

--- puphpet/files/startup-once/install-psx-sample.sh
+++ puphpet/files/startup-once/install-psx-sample.sh
@@ -1,3 +1,9 @@
+# automatically created by the apache vhost. Must be removed since composer can
+# create a project only in an empty folder
+rm -r /var/www/psx/public
+# create composer psx sample project
 composer create-project psx/sample /var/www/psx
+# replace configuration
 cp /vagrant/configuration.php /var/www/psx/configuration.php
+# activate htaccess
 mv /var/www/psx/public/htaccess.sample /var/www/psx/public/.htaccess
